cefcdc7a69c3de5f7a89372b01992ef83d30502c
sfritsen <sfritsen@gmail.com>
2022-12-16 23:12:27 -0700

    Added import file to script

--- css/script.js
+++
@@ -1,5 +0,0 @@
-// Test data
-const data_internet = [
-    { id: 'hs1', description: 'Internet 1', cost: 50 },
-    { id: 'hs2', description: 'Internet 2', cost: 60 }
-];
---
+++ data/internet.js
@@ -0,0 +1,25 @@
+// const data_internet = [
+//     {
+//         id: "hs6",
+//         description: "Internet 6",
+//         base_cost: 80,
+//     },
+//     {
+//         id: "hs15",
+//         description: "Internet 15",
+//         base_cost: 90,
+//     },
+//     {
+//         id: "hs25",
+//         description: "Internet 25",
+//         base_cost: 95,
+//     }
+// ]
+
+const data_internet = {
+    id: "hs6",
+    description: "Internet 6",
+    base_cost: 80,
+};
+
+export {data_internet};
